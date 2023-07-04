179d2560fb4ecb8c05b989c56a0b45e3ba1f99de
Attila fawal <attila_faweli@hotmail.com>
2023-07-04 21:40:02 +0000

    debug false

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -39,7 +39,7 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = [
     '8000-attilafawal-restaurantr-0tx83zl54o0.ws-us101.gitpod.io',
-    'restaurant-reservations-system-5f0577a30c22.herokuapp.com',
+    'restaurant-reservations-system.herokuapp.com',
     'localhost'
 ]
 

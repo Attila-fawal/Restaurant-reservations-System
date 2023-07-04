5b61112a59ca4da3f25830f2c874349839301e9c
Attila fawal <attila_faweli@hotmail.com>
2023-07-04 09:53:31 +0000

    Update settings, Procfile and requirements for Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn restaurant.wsgi
+web: python manage.py collectstatic --no-input; gunicorn restaurant.wsgi
--- restaurant/settings.py
+++ restaurant/settings.py
@@ -18,6 +18,9 @@ from dotenv import load_dotenv
 import cloudinary    
 
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
